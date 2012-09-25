a5185cdd7a15ec60888873f32d7ff3f82832534d
smirea <steven.mirea@gmail.com>
2012-09-25 15:16:08 +0200

    fixDate()

--- tmp.php
+++ tmp.php
@@ -8,8 +8,32 @@ $q = "select account, fname, lname from jPeople.Search where lname in ('mirea', 
 $q = mysql_query($q);
 //while( $r = mysql_fetch_assoc( $q ) ){ echo $r['account'].","; }
 
-flipDate('booked');
-flipDate('returned');
+//flipDate('booked');
+//flipDate('returned');
+
+fixDate('booked');
+//fixDate('returned');
+
+function fixDate ($column) {
+  $query = "SELECT * FROM items WHERE $column LIKE '____._._' ".
+                                  "OR $column LIKE '____.__._' ".
+                                  "OR $column LIKE '____._.__'";
+  $query = mysql_query($query);
+  while ($row = mysql_fetch_assoc($query)) {
+    $arr = explode('.', $row[$column]);
+    foreach ($arr as $key => $value) {
+      if (strlen($value) == 1) {
+        $arr[$key] = '0' . $arr[$key];
+      }
+    }
+    $newDate = implode('.', $arr);
+    //echo $row['id'] .'->'. $newDate;
+    $q = "UPDATE items SET $column='$newDate' WHERE id='".$row['id']."'";
+    if (!mysql_query($q)) {
+      echo "[Error (id:".$row['id'].")] ".mysql_error() . "\n";
+    }
+  }
+}
 
 function flipDate ($column) {
   $query = mysql_query("SELECT * FROM items WHERE $column LIKE '%.2012' OR $column LIKE '%.2011'");
